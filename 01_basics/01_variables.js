9c8b341f6396daa6116cc1782dccaa0f787ecc1c
Parth09-2003 <151987098+Parth09-2003@users.noreply.github.com>
2025-06-11 06:01:01 +0000

    variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 14453
+let accountEmail = "parthpingale12@gmail.com"
+var accountPassword ="1234"
+accountCity ="Nashik"
+let accountState = "Maharashtra"
+// accountId = 2 not allowed
+
+
+
+accountEmail = "ndsban@gmail.com"
+accountPassword = "212121"
+accountCity = "mumbai"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+//DO NOT USE VAR AS A VARIABLE 
+
